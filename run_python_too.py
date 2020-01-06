221aec5954f2d88604c47823b14201d86b3822f3
hchiam <hchiam@users.noreply.github.com>
2020-01-06 11:00:17 -0500

    test Travis CI: both .py & .js

---
+++ run_python_too.py
@@ -0,0 +1,2 @@
+# just testing whether Travis CI can handle running both Python and JavaScript
+print('\n*******************************\nTesting running Python code too\n*******************************\n')
